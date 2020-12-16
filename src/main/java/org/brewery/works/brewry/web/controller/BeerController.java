0daf4194e1ab759d625169992cbfa0c65157042f
hmnshgpt455 <hmnshgpt455@gmail.com>
2020-12-16 09:41:48 +0530

    Adding more endpoints in controller

--- src/main/java/org/brewery/works/brewry/web/controller/BeerController.java
+++ src/main/java/org/brewery/works/brewry/web/controller/BeerController.java
@@ -1,14 +1,11 @@
 package org.brewery.works.brewry.web.controller;
 
-import org.apache.coyote.Response;
 import org.brewery.works.brewry.web.model.BeerDto;
 import org.brewery.works.brewry.web.services.BeerService;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -25,5 +22,21 @@ public class BeerController {
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId) {
         return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
+    }
+
+    @PostMapping //POST - Create a new beer
+    public ResponseEntity handlePost(@RequestBody BeerDto beerDto) {
+        BeerDto savedDto = beerService.saveNewBeer(beerDto);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/beer/" + savedDto.getId().toString());
+        return new ResponseEntity(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity handleUpdate(@RequestBody BeerDto beerDto, @PathVariable("beerId") UUID beerId) {
+        beerService.updateBeer(beerId, beerDto);
+
+        //It means that server is saying I understood your request and the update happened successfully. There is no content to return to you.
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
